1a81681de4469a8899b48e3074adf37f5c8a3867
Atrolide <loszewskigrzegorz@gmail.com>
2023-03-29 13:47:29 +0200

    Updated an approach tp gather data from a specidic URL

--- tests/ototest.py
+++ tests/ototest.py
@@ -1,17 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.otomoto.pl/oferta/volkswagen-golf-salon-pl-raport-autodna-oryginalny-przebieg-2-komplety-kol-ID6Fp0pp.html'
+# Set MAXHEADERS to 1000 to get rid of HTTP Exception error
+import http.client
+
+http.client._MAXHEADERS = 1000
+
+url = 'https://www.otomoto.pl/oferta/ford-f150-ford-f150-ID6FpibS.html'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-elements_div = soup.find('div', {'class': 'offer-params with-vin'})
+# Parent div
+parent_div = soup.find('div', {'class': 'flex-container-main'})
 
-for ul in elements_div.find_all('ul', {'class': 'offer-params__list'}):
-    for li in ul.find_all('li', {'class': 'offer-params__item'}):
-        value_div = li.find('div', {'class': 'offer-params__value'})
-        value_a = value_div.find('a', {'class': 'offer-params__link'})
-        value = value_a.text.strip()
-        print(value)
+# Successive access to `div` tags
+child1_div = parent_div.find('div', {'class': 'flex-container-main__left'})
+child2_div = child1_div.find('div', {'class': 'offer-content offer-content--secondary'})
+child3_div = child2_div.find('div', {'class': 'offer-content__row om-offer-main'})
+child4_div = child3_div.find('div', {'class': 'offer-content__main-column'})
+child5_div = child4_div.find('div', {'class': 'parametersArea'})
+child6_div = child5_div.find('div', {'class': 'offer-params with-vin'})
+
+# Access to `ul` tag
+child7_ul = child6_div.find('ul', {'class': 'offer-params__list'})
+
+# Access to `li` tags
+child8_li = child7_ul.find_all('li', {'class': 'offer-params__item'})
+
+brand = ''
+model = ''
+version = ''
+year = ''
+mileage = ''
+engine_size = ''
+fuel_type = ''
+horse_power = ''
+
+for child in range(0, len(child8_li)):
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Marka pojazdu':
+        brand = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Model pojazdu':
+        model = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Wersja':
+        version = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Rok produkcji':
+        year = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Przebieg':
+        mileage = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Pojemność skokowa':
+        engine_size = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Rodzaj paliwa':
+        fuel_type = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+    if child8_li[child].find('span', {'class': 'offer-params__label'}).text.strip() == 'Moc':
+        horse_power = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
+
+print(brand, model, version, year, mileage, engine_size, fuel_type, horse_power)
 
