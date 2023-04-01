efbb436279d9402b76d92ff159e26cd8496aaa44
Atrolide <loszewskigrzegorz@gmail.com>
2023-04-01 10:58:55 +0200

    Changed accessing to child objects approach to a 1liner

--- tests/ototest.py
+++ tests/ototest.py
@@ -6,7 +6,7 @@ import http.client
 
 http.client._MAXHEADERS = 1000
 
-url = 'https://www.otomoto.pl/oferta/ford-f150-ford-f150-ID6FpibS.html'
+url = 'https://www.otomoto.pl/oferta/audi-a3-audi-a3-8v-1-6-tdi-s-line-led-navi-ID6EFDRd.html'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -14,20 +14,21 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # Parent div
 parent_div = soup.find('div', {'class': 'flex-container-main'})
 
-# Successive access to `div` tags
-child1_div = parent_div.find('div', {'class': 'flex-container-main__left'})
-child2_div = child1_div.find('div', {'class': 'offer-content offer-content--secondary'})
-child3_div = child2_div.find('div', {'class': 'offer-content__row om-offer-main'})
-child4_div = child3_div.find('div', {'class': 'offer-content__main-column'})
-child5_div = child4_div.find('div', {'class': 'parametersArea'})
-child6_div = child5_div.find('div', {'class': 'offer-params with-vin'})
+final_child_div = parent_div.find('div', {'class': 'flex-container-main__left'})\
+                    .find('div', {'class': 'offer-content offer-content--secondary'})\
+                    .find('div', {'class': 'offer-content__row om-offer-main'})\
+                    .find('div', {'class': 'offer-content__main-column'})\
+                    .find('div', {'class': 'parametersArea'})\
+                    .find('div', {'class': 'offer-params with-vin'})
+
 
 # Access to `ul` tag
-child7_ul = child6_div.find('ul', {'class': 'offer-params__list'})
+child7_ul = final_child_div.find('ul', {'class': 'offer-params__list'})
 
 # Access to `li` tags
 child8_li = child7_ul.find_all('li', {'class': 'offer-params__item'})
 
+# Predefine each variable as blank
 brand = ''
 model = ''
 version = ''
@@ -63,4 +64,3 @@ for child in range(0, len(child8_li)):
         horse_power = child8_li[child].find('div', {'class': 'offer-params__value'}).text.strip()
 
 print(brand, model, version, year, mileage, engine_size, fuel_type, horse_power)
-
